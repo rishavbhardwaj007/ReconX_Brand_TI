83d0b0ed598afdd7a55693367d277210cb1a1e22
Rishav Bhardwaj <rishavbhardwaj26@gmail.com>
2025-07-20 14:50:18 +0530

    Fixed import path for telegram_utils

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from utils.github import search_github_leaks
-from telegram_utils import send_telegram_alert
+from utils.telegram_utils import send_telegram_alert
 from dotenv import load_dotenv
 load_dotenv()
 
